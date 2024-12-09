fe29678371ecce0caa0925399175d04564ffb892
Michał Trawczyński <01171684@pw.edu.pl>
2024-12-09 11:35:29 +0100

    Rozwiązanie zadania 6

--- foo.hpp
+++ foo.hpp
@@ -10,7 +10,13 @@ std::vector< char > foo(std::list< Human >& people)
 {
     std::vector< char > retval(people.size());
 
-    // Twoja implementacja tutaj
+    // Inkrementacja wieku za pomoc¹ metody `birthday`
+    std::for_each(people.begin(), people.end(), [](Human& person) { person.birthday(); });
+
+    // Odwrócenie kolejnoœci i zapisanie znaków 'y' lub 'n'
+    std::transform(people.rbegin(), people.rend(), retval.begin(), [](const Human& person) {
+        return person.isMonster() ? 'n' : 'y';
+    });
 
     return retval;
 }
